32641521f3601b4ea6ea1fa0994fd5a46dc98f8c
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-04-10 12:57:43 -0700

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+//import 'package:auth_widget_builder/auth_widget_builder.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:DigitalTraveler/app/auth_widget.dart';
+import 'package:DigitalTraveler/app/home/home_page.dart';
+import 'package:DigitalTraveler/app/onboarding/onboarding_page.dart';
+import 'package:DigitalTraveler/app/onboarding/onboarding_view_model.dart';
+import 'package:DigitalTraveler/app/top_level_providers.dart';
+import 'package:DigitalTraveler/app/sign_in/sign_in_page.dart';
+import 'package:DigitalTraveler/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:DigitalTraveler/services/shared_preferences_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(ProviderScope(
+    overrides: [
+      sharedPreferencesServiceProvider.overrideWithValue(
+        SharedPreferencesService(sharedPreferences),
+      ),
+    ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseAuth = context.read(firebaseAuthProvider);
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: AuthWidget(
+        nonSignedInBuilder: (_) => Consumer(
+          builder: (context, watch, _) {
+            final didCompleteOnboarding =
+                watch(onboardingViewModelProvider.state);
+            return didCompleteOnboarding ? SignInPage() : OnboardingPage();
+          },
+        ),
+        signedInBuilder: (_) => HomePage(),
+      ),
+      onGenerateRoute: (settings) =>
+          AppRouter.onGenerateRoute(settings, firebaseAuth),
+    );
+  }
+}
