c2d4b993c08135bda40c4e46d4672cadb0fc0c82
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-04-10 13:06:34 -0700

    Cleaned up stuff from starter repo

--- lib/app/home/entries/entry_job.dart
+++
@@ -1,9 +0,0 @@
-import 'package:DigitalTraveler/app/home/models/entry.dart';
-import 'package:DigitalTraveler/app/home/models/job.dart';
-
-class EntryJob {
-  EntryJob(this.entry, this.job);
-
-  final Entry entry;
-  final Job job;
-}
