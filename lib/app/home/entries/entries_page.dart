d70ae1fd1ebcb0234783b5a9a7a368d3dfb37b32
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-04-11 23:04:59 -0700

    Revert "Cleaned up stuff from starter repo"
    This reverts commit c2d4b993c08135bda40c4e46d4672cadb0fc0c82.

---
+++ lib/app/home/entries/entries_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:DigitalTraveler/app/home/entries/entries_view_model.dart';
+import 'package:DigitalTraveler/app/home/entries/entries_list_tile.dart';
+import 'package:DigitalTraveler/app/home/jobs/list_items_builder.dart';
+import 'package:DigitalTraveler/app/top_level_providers.dart';
+import 'package:DigitalTraveler/constants/strings.dart';
+
+final entriesTileModelStreamProvider =
+    StreamProvider.autoDispose<List<EntriesListTileModel>>(
+  (ref) {
+    final database = ref.watch(databaseProvider);
+    final vm = EntriesViewModel(database: database);
+    return vm.entriesTileModelStream;
+  },
+);
+
+class EntriesPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(Strings.entries),
+        elevation: 2.0,
+      ),
+      body: _buildContents(context, watch),
+    );
+  }
+
+  Widget _buildContents(BuildContext context, ScopedReader watch) {
+    final entriesTileModelStream = watch(entriesTileModelStreamProvider);
+    return ListItemsBuilder<EntriesListTileModel>(
+      data: entriesTileModelStream,
+      itemBuilder: (context, model) => EntriesListTile(model: model),
+    );
+  }
+}
